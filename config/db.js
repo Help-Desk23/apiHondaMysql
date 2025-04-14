c3d0d5ce46aade32c7c85e6de9d93c13638fc5da
ASistemasVian <auxiliarsistemasvian@gmail.com>
2025-04-14 14:41:51 -0400

    Update  variable of db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+/*const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
@@ -6,6 +6,31 @@ const db = mysql.createConnection({
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error conectando a MySQL:', err);
+        return;
+    }
+    console.log('Conectado a la base de datos correctamente.');
+});
+
+module.exports = db;*/
+
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection({
+    user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT),
+  options: {
+    encrypt: false,
+    trustServerCertificate: true
+  }
 });
 
 db.connect((err) => {
