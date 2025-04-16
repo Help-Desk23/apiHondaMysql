b508120d8a9eca40427b6f0825ae82e0552adb83
ASistemasVian <auxiliarsistemasvian@gmail.com>
2025-04-16 10:50:36 -0400

    Update db.js
    Cambiando de createconnection a create poll

--- config/db.js
+++ config/db.js
@@ -21,12 +21,17 @@ module.exports = db;*/
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,        // <-- corregido
+const db = mysql.createPool({
+  host: process.env.DB_HOST,       
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: parseInt(process.env.DB_PORT)
+  port: parseInt(process.env.DB_PORT),
+  //agregar opciones de pool
+  waitForConnections: true,
+  connectionLimit: 10,    
+  queueLimit: 0
+  // Puedes ajustar este número según lo necesites
 });
 
 db.connect((err) => {
