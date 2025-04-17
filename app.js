0ae55a2187b5cd071464d9cd53e1390fd574bc33
Help-Desk23 <vian.sistemas@gmail.com>
2025-04-17 15:08:41 -0400

    Correccion de Error

--- app.js
+++ app.js
@@ -173,7 +173,7 @@ io.on('connection', (socket) => {
   socket.on('obtenerMotos', () => getMotos(socket));
   socket.on('obtenerProformas', () => getProformas(socket));
   socket.on('obtenerCotizacion', () => getCotizacion(socket));
-  socket.on('obtenerCotizacionAsesor',(id_asesores) => getCotizacionAsesor(socket, id_asesores));
+  socket.on('obtenerCotizacionAsesor',({id_asesores}) => getCotizacionAsesor(socket, id_asesores));
   socket.on('obtenerClientes', () => getClientes(socket));
 
   socket.on('disconnect', () => {
