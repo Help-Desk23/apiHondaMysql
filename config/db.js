7400ab6b54ec03668b2e9e5a838ae2f94762baec
Help-Desk23 <vian.sistemas@gmail.com>
2025-04-25 10:26:38 -0400

    Se agrego Descuento Inicial a la Tabla CostoVarios

--- config/db.js
+++ config/db.js
@@ -17,3 +17,21 @@ db.connect((err) => {
 });
 
 module.exports = db;
+
+/*const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,       
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT),
+  //agregar opciones de pool
+  waitForConnections: true,
+  connectionLimit: 10,    
+  queueLimit: 0
+  // Puedes ajustar este número según lo necesites
+});
+
+module.exports = db;*/
